bf8c2953b53a066f5f41328c2be2d1162c36a596
Joel Cruz <joel@umbrumbralshm@gmail.com>
2026-01-17 01:16:54 -0600

    Switch database to PostgreSQL (Render)

--- app/database.py
+++ app/database.py
@@ -6,20 +6,21 @@ Created on Fri Jan 16 23:46:31 2026
 """
 
 # app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-
-DB_PATH = os.environ.get("DB_PATH", "/data/shm.db")
 DATABASE_URL = os.environ["DATABASE_URL"]
-
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
